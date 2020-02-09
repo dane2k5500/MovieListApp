439cf5e83be92cd42eb0b8a1ca7e5e0423815236
dane2k5500 <diework10@gmail.com>
2020-02-09 12:52:55 -0600

    Updates

---
+++ precache-manifest.ac26b767c2c22bc1a5e6a2d2a80dd3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1515977294eef1dfb02f595ceca78c",
+    "url": "/MovieListApp/index.html"
+  },
+  {
+    "revision": "c9a2645f56e449e5266a",
+    "url": "/MovieListApp/static/css/main.bee0b98d.chunk.css"
+  },
+  {
+    "revision": "c069434b13301ae4d589",
+    "url": "/MovieListApp/static/js/2.afcc1577.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MovieListApp/static/js/2.afcc1577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a2645f56e449e5266a",
+    "url": "/MovieListApp/static/js/main.b7f8ef03.chunk.js"
+  },
+  {
+    "revision": "d1ab27a193048e261142",
+    "url": "/MovieListApp/static/js/runtime-main.7124478e.js"
+  }
+]);
